c843ba4d34676dd97caec99d2b3f936ed444f20a
Robu David <david@devcentrehouse.eu>
2025-05-04 16:46:35 +0300

    Added vercel speed insights

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,15 @@
-import { Fragment } from 'react';
-import 'react-loading-skeleton/dist/skeleton.css';
-import { MouseSmooth } from 'react-mouse-smooth';
-import { RouterProvider } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Import necessary parts from React Query
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // For dev tools
-import routes from 'configs/routes';
-import dayjs from 'dayjs';
-import format from 'dayjs/plugin/customParseFormat';
-import 'react-toastify/dist/ReactToastify.css';
+import { Fragment } from "react";
+import "react-loading-skeleton/dist/skeleton.css";
+import { MouseSmooth } from "react-mouse-smooth";
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import necessary parts from React Query
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // For dev tools
+import routes from "configs/routes";
+import dayjs from "dayjs";
+import format from "dayjs/plugin/customParseFormat";
+import "react-toastify/dist/ReactToastify.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 dayjs.extend(format);
 
@@ -29,10 +30,11 @@ function App(): JSX.Element {
         <ReactQueryDevtools
           client={queryClient}
           initialIsOpen={false}
-          position='right'
-          buttonPosition='bottom-right'
+          position="right"
+          buttonPosition="bottom-right"
         />
       </QueryClientProvider>
+      <SpeedInsights />
     </Fragment>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 // Copyright (c) Jonathan Ferraz.
 // Licensed under the MIT license.
 
-import ReactDOM from 'react-dom/client';
-import { Fragment } from 'react/jsx-runtime';
-import 'assets/styles/_index.scss';
-import 'assets/styles/default.scss';
+import ReactDOM from "react-dom/client";
+import { Fragment } from "react/jsx-runtime";
+import "assets/styles/_index.scss";
+import "assets/styles/default.scss";
 
-import App from 'app';
+import App from "app";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <Fragment>
     <App />
   </Fragment>
