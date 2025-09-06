923c27ff94ed656a531536518e4fe3f2d0813051
Robu David <david@devcentrehouse.eu>
2025-09-06 22:55:32 +0300

    Update scrambleTicker.tsx

--- src/components/scrambleTicker/scrambleTicker.tsx
+++ src/components/scrambleTicker/scrambleTicker.tsx
@@ -1,10 +1,10 @@
 // Copyright (c) Jonathan Ferraz.
 // Licensed under the MIT license.
 
-import dayjs from 'dayjs';
-import { useEffect, useState, type ReactHTML } from 'react';
+import dayjs from "dayjs";
+import { useEffect, useState, type ReactHTML } from "react";
 
-import ScrambleText from '../scrambleText/scrambleText';
+import ScrambleText from "../scrambleText/scrambleText";
 
 type ScrambleTickerProps<Tag extends keyof ReactHTML> = {
   tag?: Tag;
@@ -18,10 +18,10 @@ export default function ScrambleTicker<Tag extends keyof ReactHTML>({
   tag,
   duration = 0.6,
   intervalMs = 1500,
-  formats = ['HH:mm:ss', 'YYYY-MM-DD HH:mm', 'DD MMM HH:mm'],
+  formats = ["HH:mm:ss", "YYYY-MM-DD HH:mm", "DD MMM HH:mm"],
   values,
 }: ScrambleTickerProps<Tag>) {
-  const [idx, setIdx] = useState(0);
+  const [, setIdx] = useState(0);
   const [text, setText] = useState<string>(
     values && values.length > 0 ? values[0] : dayjs().format(formats[0])
   );
